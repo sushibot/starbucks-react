ec890415a411c24bb336dfd78c5112762eb268c1
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2022-02-17 16:12:34 -1000

    feat: create header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React from "react";
 
-export default function Header({className, children}){
-    return <header className={className}>{children}</header>
+export default function Header({ className, children }) {
+  return (
+    <>
+      <header className={className}>
+        {children}
+        <section
+          style={{
+            backgroundColor: "var(--primary-100)",
+            height: "50px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              maxWidth: "1440px",
+              width: "100%",
+            }}
+          >
+            <p className="text-white font-weight-700 text-uppercase">
+              Starbucks® Rewards
+            </p>
+          </div>
+        </section>
+      </header>
+    </>
+  );
 }
